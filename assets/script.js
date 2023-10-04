41513c78875a3f0f723b4219ba5331cc9899c89f
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-03 19:26:24 -0700

    cleaned up html

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,17 @@ const searchHistory = document.getElementById('search-history');
 const currentWeather = document.getElementById('current-weather');
 const forecast = document.getElementById('forecast');
 
+//https://api.openweathermap.org/data/2.5/
+
+//weather?q=las%20vegas&appid=c2332ab353784f603d2622928b313f68
+//forecast?lat=36.175&lon=-115.1372&cnt=5&appid=c2332ab353784f603d2622928b313f68
+
+
 // Function to fetch weather data from an API
  function fetchWeatherData(city) {
-    var queryURL = baseUrl + '/weather?q=' + city + '&appid=' + APIKey;
+    var queryURL = baseUrl + '/weather?q=' + city + '&units=imperial&appid=' + APIKey;
+
+    var lat,lon;
 
     fetch(queryURL).then((response) => {
         console.log(response)
@@ -20,20 +28,25 @@ const forecast = document.getElementById('forecast');
             document.getElementById('todayTemp').innerHTML = data.main.temp
             document.getElementById('todayHumidity').innerHTML = data.main.humidity
             document.getElementById('todayWind').innerHTML = data.wind.speed
+
+            lat = 36.175;
+            lon = -115.1372;
+
+            fetchForecast(lat,lon)
         });
-        
-        
     });
+
+
 }
 
 function fetchForecast(lat,lon){
-    var queryURL = baseUrl + '/forecast/daily?lat=' + lat + '&lon=' + lon + '&cnt=5&appid=' + APIKey;
+
+    var queryURL = baseUrl + '/forecast?lat=' + lat + '&lon=' + lon + '&cnt=5&units=imperial&appid=' + APIKey;
 
     fetch(queryURL).then((response) => {
         console.log(response)
         response.json().then((data) => {
             console.log(data);
-            
         });
         
         
