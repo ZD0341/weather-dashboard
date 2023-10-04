cc94c225d119169de10ce27175b8a4b1d12e32e2
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-03 18:36:16 -0700

    testing

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
-var APIkey = "c2332ab353784f603d2622928b313f68";
-
- // Function to fetch weather data from an API
- function fetchWeatherData(city) {
-   
-}
+var APIKey = "c2332ab353784f603d2622928b313f68";
+//https://api.openweathermap.org/data/2.5/weather?q=LasVegas&appid=c2332ab353784f603d2622928b313f68
+var baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
 
 const cityInput = document.getElementById('city-input');
 const searchButton = document.getElementById('search-button');
@@ -11,8 +8,27 @@ const searchHistory = document.getElementById('search-history');
 const currentWeather = document.getElementById('current-weather');
 const forecast = document.getElementById('forecast');
 
+// Function to fetch weather data from an API
+ function fetchWeatherData(city) {
+    var queryURL = baseUrl + '?q=' + city + "&appid=" + APIKey;
+
+    fetch(queryURL).then((response) => {
+        console.log(response)
+        response.json().then((data) => {
+            console.log(data);
+            document.getElementById('searchedCity').innerHTML = 'test'
+            document.getElementById('todayTemp').innerHTML = 'test'
+            document.getElementById('todayHumidity').innerHTML = 'test'
+            document.getElementById('todayWind').innerHTML = 'test'
+        });
+        weatherToday = document.getElementById('weather-today')
+        
+    });
+}
+
+
 searchButton.addEventListener('click', () => {
-    const city = cityInput.value;
+    const city = cityInput.value.toLowerCase();
     if (city.trim() !== '') {
         // Call the fetchWeatherData function with the entered city
         fetchWeatherData(city);
