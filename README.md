67b9946f0d3a6f7ffed2d52de3c22dbcac888fd0
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-03 21:07:44 -0700

    final draft

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # weather-dashboard
 
 ##description
-application gives you the current weather of a given city and a 5 day forcast
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
 
 takeaways:
+This was a fun challenge to work on, I learned alot about API's and JS.
+
+I added some spooky themes in honor of halloween coming up!
+
--- assets/script.js
+++ assets/script.js
@@ -24,13 +24,15 @@ const forecast = document.getElementById('forecast');
         console.log(response)
         response.json().then((data) => {
             console.log(data);
+            var date = new Date().toLocaleDateString()
+            document.getElementById('todayDate').innerHTML = "(" + date + ")"
             document.getElementById('searchedCity').innerHTML = data.name
             document.getElementById('todayTemp').innerHTML = data.main.temp
             document.getElementById('todayHumidity').innerHTML = data.main.humidity
             document.getElementById('todayWind').innerHTML = data.wind.speed
 
-            lat = 36.175;
-            lon = -115.1372;
+            lat = data.coord.lat;
+            lon = data.coord.lon;
 
             fetchForecast(lat,lon)
         });
@@ -41,12 +43,56 @@ const forecast = document.getElementById('forecast');
 
 function fetchForecast(lat,lon){
 
-    var queryURL = baseUrl + '/forecast?lat=' + lat + '&lon=' + lon + '&cnt=5&units=imperial&appid=' + APIKey;
+    var queryURL = baseUrl + '/forecast?lat=' + lat + '&lon=' + lon + '&cnt=40&units=imperial&appid=' + APIKey;
 
     fetch(queryURL).then((response) => {
         console.log(response)
         response.json().then((data) => {
             console.log(data);
+            var date1 = data.list[0].dt_txt.split(' ')[0].split('-')
+            var year1 = date1[0]
+            var month1 = date1[1]
+            var day1 = date1[2]
+            document.getElementById('fiveDayDate1').innerHTML = month1 + "/" + day1 + "/" + year1
+            document.getElementById('fiveDayTemp1').innerHTML = data.list[0].main.temp
+            document.getElementById('fiveDayHumidity1').innerHTML = data.list[0].main.humidity
+            document.getElementById('fiveDayWind1').innerHTML = data.list[0].wind.speed
+            
+            var date2 = data.list[7].dt_txt.split(' ')[0].split('-')
+            var year2 = date2[0]
+            var month2 = date2[1]
+            var day2 = date2[2]
+            document.getElementById('fiveDayDate2').innerHTML = month2 + "/" + day2 + "/" + year2
+            document.getElementById('fiveDayTemp2').innerHTML = data.list[1].main.temp
+            document.getElementById('fiveDayHumidity2').innerHTML = data.list[1].main.humidity
+            document.getElementById('fiveDayWind2').innerHTML = data.list[1].wind.speed
+            
+            var date3 = data.list[15].dt_txt.split(' ')[0].split('-')
+            var year3 = date3[0]
+            var month3 = date3[1]
+            var day3 = date3[2]
+            document.getElementById('fiveDayDate3').innerHTML = month3 + "/" + day3 + "/" + year3
+            document.getElementById('fiveDayTemp3').innerHTML = data.list[2].main.temp
+            document.getElementById('fiveDayHumidity3').innerHTML = data.list[2].main.humidity
+            document.getElementById('fiveDayWind3').innerHTML = data.list[2].wind.speed
+            
+            var date4 = data.list[23].dt_txt.split(' ')[0].split('-')
+            var year4 = date4[0]
+            var month4 = date4[1]
+            var day4 = date4[2]
+            document.getElementById('fiveDayDate4').innerHTML = month4 + "/" + day4 + "/" + year4
+            document.getElementById('fiveDayTemp4').innerHTML = data.list[3].main.temp
+            document.getElementById('fiveDayHumidity4').innerHTML = data.list[3].main.humidity
+            document.getElementById('fiveDayWind4').innerHTML = data.list[3].wind.speed
+            
+            var date5 = data.list[31].dt_txt.split(' ')[0].split('-')
+            var year5 = date5[0]
+            var month5 = date5[1]
+            var day5 = date5[2]
+            document.getElementById('fiveDayDate5').innerHTML = month5 + "/" + day5 + "/" + year5
+            document.getElementById('fiveDayTemp5').innerHTML = data.list[4].main.temp
+            document.getElementById('fiveDayHumidity5').innerHTML = data.list[4].main.humidity
+            document.getElementById('fiveDayWind5').innerHTML = data.list[4].wind.speed
         });
         
         
